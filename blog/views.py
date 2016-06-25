b90924939f688204d8d3b88a2d582a3fe9d59961
Jacob Burch <jacob@burch.io>
2016-06-25 14:17:51 -0500

    finish

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from django.utils import timezone
 
-# Create your views here.
+from blog.models import Post
+
+
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    context_data = {
+        'posts': posts
+    }
+    return render(request, 'blog/post_list.html', context_data)
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    context_data = {
+        'post': post
+    }
+    return render(request, 'blog/post_detail.html', context_data)
